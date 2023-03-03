95c44122163e872bf3b96374c42460465a2d0a88
leonpiedrahita <leondpr@gmail.com>
2023-03-02 21:23:45 -0500

    v-bind

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup>
-  const name = `vue dinamico`
+  const name = `vue dinamico`;
+  const styleColor = "color: blue";
 </script>
 
 <template>
   <h1>Hola {{name}}</h1>
+  <h2 v-bind:style="styleColor"> Soy azul </h2>
 </template>
 
 <style>
