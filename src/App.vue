752096f3ff836da73438242ff6ceac19e2957ae3
leonpiedrahita <leondpr@gmail.com>
2023-03-02 21:34:04 -0500

    v-if

--- src/App.vue
+++ src/App.vue
@@ -9,6 +9,10 @@
   <h2 v-bind:style="styleColor"> Soy azul </h2>
   <h3 :style="styleColor"> Soy azul </h3>
    <h3> {{ activo ? "Estoy activo":"Estoy inactivo" }}</h3> <!--Operador ternario de comparacion -->
+  <p v-if="!activo">Estoy inactivo</p>
+
+
+
 </template>
 
 <style>
