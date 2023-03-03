60f3d84cb94db29d097299c7aad5cbef47fdb793
leonpiedrahita <leondpr@gmail.com>
2023-03-02 21:32:02 -0500

    operador ternario de comparacion

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,14 @@
 <script setup>
   const name = `vue dinamico`;
   const styleColor = "color: blue";
+  const activo = false;
 </script>
 
 <template>
-  <h1>Hola {{name}}</h1>
+  <h1>Hola {{name.toUpperCase()}}</h1>
   <h2 v-bind:style="styleColor"> Soy azul </h2>
+  <h3 :style="styleColor"> Soy azul </h3>
+   <h3> {{ activo ? "Estoy activo":"Estoy inactivo" }}</h3> <!--Operador ternario de comparacion -->
 </template>
 
 <style>
